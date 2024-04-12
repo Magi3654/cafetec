da8bf6c4a3e331836c4d5a9cf0d00aa100c8d7c5
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-04-12 02:30:50 -0700

    add logo

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-yellow h-screen flex flex-col justify-center items-center">
-     
       <Image
         src={'/img/ondasBlanco.png'}
         width={0}
@@ -14,8 +13,18 @@ export default function Home() {
 
       </Image>
 
-      <p>Logo</p>
-      <button type="button" className="text-white bg-brown focus:ring-2 focus:outline-none font-medium rounded-2xl text-md px-8 py-2.5 text-center inline-flex items-center me-2 mb-2">
+      <Image
+        src={'/img/logo.png'}
+        width={0}
+        height={0}
+        alt=""
+        sizes="100vw"
+        className="h-64 w-64">
+      </Image>
+
+      <p className="text-white m-2">"Tu comida favorita, lista para llevar!"</p>
+
+      <button type="button" className="text-white bg-brown focus:ring-2 focus:outline-none font-medium rounded-2xl text-md px-8 py-2.5 text-center inline-flex items-center m-3">
         INICIAR
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#fff" className="h-4 ml-2">
           <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
