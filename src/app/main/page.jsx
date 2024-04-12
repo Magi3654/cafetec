c302a71ca44639680f0e386a042cc8721d3bf2a7
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2024-04-12 02:58:20 -0700

    add main page

---
+++ src/app/main/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className="bg-white">
+            <div className="flex flex-col">
+                <p className="text-brown">Welcome</p>
+                <p className="text-brown">CafeTec</p>
+            </div>
+        </div>
+    )
+}
+
+export default Main
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,7 +12,7 @@ export default function Home() {
         height={0}
         alt=""
         sizes="100vw"
-        className="absolute top-0 left-0 w-full h-96">
+        className="absolute top-0 left-0 w-full h-60 mb-7">
 
       </Image>
 
@@ -24,12 +27,14 @@ export default function Home() {
 
       <p className="text-white m-2">"Tu comida favorita, lista para llevar!"</p>
 
-      <button type="button" className="text-white bg-brown focus:ring-2 focus:outline-none font-medium rounded-2xl text-md px-8 py-2.5 text-center inline-flex items-center m-3">
-        INICIAR
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#fff" className="h-4 ml-2">
-          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
-        </svg>
-      </button>
+      <Link href="/main">
+        <button type="button" className="text-white bg-brown focus:ring-2 focus:outline-none font-medium rounded-2xl text-md px-8 py-2.5 text-center inline-flex items-center m-3">
+          INICIAR
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#fff" className="h-4 ml-2">
+            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+          </svg>
+        </button>
+      </Link>
 
       <Image
           src={'/img/ondasAbajo.png'}
@@ -37,7 +42,7 @@ export default function Home() {
           height={0}
           alt=""
           sizes="100vw"
-          className="absolute bottom-0 left-0 w-full h-96">
+          className="absolute bottom-0 left-0 w-full h-60 mt-7">
       </Image>
     </main>
   );
